ae3386e08c0a5086ed4fbc9bf51bc8cdc2d3787c
ines <bourhim.ines@gmail.com>
2024-03-04 13:41:56 +0100

    exemple de fonctions

--- src/App.js
+++ src/App.js
@@ -3,24 +3,61 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let myFunc = ()=>{
+    console.log("Execute de myFunc");
+    return true;
+  };
+  
+  myFunc;
+
+  let jsonObj = {
+    varOne: true,
+    varTwo: false,
+    varThree: "true",
+    varFour: "false",
+    varFive: 1,
+  };
+
+  let { varOne,varTwo } = jsonObj;
+
+  const getIntPlusOne = (int) => {
+    return int.varFive++;
+  };
+
+  const getIntPlusOneP = ({varFive}) => {
+    return varFive++;
+  };
+
+  const sentenceConstructor = (...params) =>{
+    let sentence = ''
+    params.map((x,i) => {
+      sentence += x
+    });
+    return sentence;
+  };
+
+  sentenceConstructor('ga','bu');
+
+  const mySecondFunc = ({varOne}, {varTwo, ...properties}) =>{
+    console.log(varOne)
+    console.log(varTwo)
+    console.log(properties.varFive)
+  }
+
+  mySecondFunc(jsonObj,jsonObj)
+
+  const crashOuFenek = (true ? console.log("Papier mon gars") : console.log("Feuilles longues comme Fei Long")) ? console.log("It's true") : console.log("It's false");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"></div>
   );
 }
+
+
+
+
+
+
 
 export default App;
